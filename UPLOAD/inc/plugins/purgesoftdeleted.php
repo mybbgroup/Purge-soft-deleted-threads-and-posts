c66c1726e600dc6906737fdbe22a9b86a7ccb9d8
Eldenroot <Eldenroot@gmail.com>
2021-02-13 13:04:57 +0100

    Re-indent code

--- UPLOAD/inc/plugins/purgesoftdeleted.php
+++ UPLOAD/inc/plugins/purgesoftdeleted.php
@@ -1,45 +1,45 @@
 <?php
-
- /**
- * MyBB Purge soft deleted posts and threads - plugin for MyBB 1.8.x forum software
- * 
- * @package MyBB Plugin
- * @author MyBB Group - Eldenroot - <eldenroot@gmail.com>
- * @copyright 2021 MyBB Group <http://mybb.group>
- * @link <https://github.com/mybbgroup/MyBB_Purge-soft-deleted-threads-and-posts>
- * @license GPL-3.0
- * 
- */
-  
-/**
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
- */
- 
-// Disallow direct access to this file for security reasons
-if(!defined("IN_MYBB"))
-{
-    die("Direct initialization of this file is not allowed.");
-}
-
-// Plugin info
-function purgesoftdeleted_info()
-{
-	global $lang;
-    $lang->load("config_purgesoftdeleted");
 	
-    return array(
+	/**
+		* MyBB Purge soft deleted posts and threads - plugin for MyBB 1.8.x forum software
+		*
+		* @package MyBB Plugin
+		* @author MyBB Group - Eldenroot - <eldenroot@gmail.com>
+		* @copyright 2021 MyBB Group <http://mybb.group>
+		* @link <https://github.com/mybbgroup/MyBB_Purge-soft-deleted-threads-and-posts>
+		* @license GPL-3.0
+		*
+	*/
+	
+	/**
+		* This program is free software: you can redistribute it and/or modify
+		* it under the terms of the GNU General Public License as published by
+		* the Free Software Foundation, either version 3 of the License,
+		* or (at your option) any later version.
+		*
+		* This program is distributed in the hope that it will be useful,
+		* but WITHOUT ANY WARRANTY; without even the implied warranty of
+		* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+		* See the GNU General Public License for more details.
+		*
+		* You should have received a copy of the GNU General Public License
+		* along with this program.
+		* If not, see <http://www.gnu.org/licenses/>.
+	*/
+	
+	// Disallow direct access to this file for security reasons
+	if(!defined("IN_MYBB"))
+	{
+		die("Direct initialization of this file is not allowed.");
+	}
+	
+	// Plugin info
+	function purgesoftdeleted_info()
+	{
+		global $lang;
+		$lang->load("config_purgesoftdeleted");
+		
+		return array(
         "name"          => $lang->purgesoftdeleted_name,
         "description"   => $lang->purgesoftdeleted_desc,
         "website"       => "https://github.com/mybbgroup/MyBB_Purge-soft-deleted-threads-and-posts",
@@ -48,54 +48,54 @@ function purgesoftdeleted_info()
         "version"       => "1.1.0",
         "codename"      => "purgesoftdeleted",
         "compatibility" => "18*"
-    );
-}
-
-// Plugin activate
-function purgesoftdeleted_activate()
-{
-	global $db, $cache, $lang;
-	$lang->load("config_purgesoftdeleted");
+		);
+	}
 	
+	// Plugin activate
+	function purgesoftdeleted_activate()
+	{
+		global $db, $cache, $lang;
+		$lang->load("config_purgesoftdeleted");
+		
 		// Create task - Purge soft deleted
 		// Have we already added this task?
-			$query = $db->simple_select('tasks', 'tid', "file='purgesoftdeleted'", array('limit' => '1'));
-			if($db->num_rows($query) == 0)
-			{
-				// Load tasks function needed to run a task and add nextrun time
-					require_once MYBB_ROOT."/inc/functions_task.php";
-				
-				// If not then add
-					$new_task = array(
-						"title" => $db->escape_string($lang->purgesoftdeleted_task_name),
-						"description" => $db->escape_string($lang->purgesoftdeleted_task_desc),
-						"file" => "purgesoftdeleted",
-						"minute" => '2',
-						"hour" => '0',
-						"day" => '*',
-						"month" => '*',
-						"weekday" => '*',
-						"enabled" => '1',
-						"logging" => '1',
-					);
-        
+		$query = $db->simple_select('tasks', 'tid', "file='purgesoftdeleted'", array('limit' => '1'));
+		if($db->num_rows($query) == 0)
+		{
+			// Load tasks function needed to run a task and add nextrun time
+			require_once MYBB_ROOT."/inc/functions_task.php";
+			
+			// If not then add
+			$new_task = array(
+			"title" => $db->escape_string($lang->purgesoftdeleted_task_name),
+			"description" => $db->escape_string($lang->purgesoftdeleted_task_desc),
+			"file" => "purgesoftdeleted",
+			"minute" => '2',
+			"hour" => '0',
+			"day" => '*',
+			"month" => '*',
+			"weekday" => '*',
+			"enabled" => '1',
+			"logging" => '1',
+			);
+			
 			$new_task['nextrun'] = fetch_next_run($new_task);
 			$tid = $db->insert_query("tasks", $new_task);
-		
-		// Update the task and run it right now
+			
+			// Update the task and run it right now
 			$cache->update_tasks();
 			run_task($tid);
-			}
-}
-
-// Plugin deactivate
-function purgesoftdeleted_deactivate()
-{
-	global $db, $cache;
-    
-	// Remove task from task manager
-	$db->delete_query('tasks', 'file=\'purgesoftdeleted\''); // Delete Purge soft deleted task
+		}
+	}
 	
-	// Update taks cache
-	$cache->update_tasks();
-}
+	// Plugin deactivate
+	function purgesoftdeleted_deactivate()
+	{
+		global $db, $cache;
+		
+		// Remove task from task manager
+		$db->delete_query('tasks', 'file=\'purgesoftdeleted\''); // Delete Purge soft deleted task
+		
+		// Update taks cache
+		$cache->update_tasks();
+	}
--- UPLOAD/inc/tasks/purgesoftdeleted.php
+++ UPLOAD/inc/tasks/purgesoftdeleted.php
@@ -1,54 +1,54 @@
 <?php
-
- /**
- * MyBB Purge soft deleted posts and threads - plugin for MyBB 1.8.x forum software
- *
- * @package MyBB Plugin
- * @author MyBB Group - Eldenroot - <eldenroot@gmail.com>
- * @copyright 2021 MyBB Group <http://mybb.group>
- * @link <https://github.com/mybbgroup/MyBB_Purge-soft-deleted-threads-and-posts>
- * @license GPL-3.0
- *
- */
-
-/**
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
-// Disallow direct access to this file for security reasons
-if(!defined("IN_MYBB"))
-{
-    die("Direct initialization of this file is not allowed.");
-}
-
-// Purge soft deleted
-function task_purgesoftdeleted($task)
-{
-    global $db, $lang;
-    $lang->load("config_purgesoftdeleted");
-
-    // Soft deleted posts and threads older than x seconds will be purged
-    $ptime = 3*24*3600; // 3 days for soft deleted posts
-    $ttime = 5*24*3600; // 5 days for soft deleted threads
-
-    $db->delete_query("posts", "(visible = -1) AND (dateline < ".(TIME_NOW-$ptime).")");
-
-    $db->delete_query("threads", "(visible = -1) AND (dateline < ".(TIME_NOW-$ttime).")");
-
-    // Optimize DB table
-    $db->query("OPTIMIZE TABLE `".TABLE_PREFIX."posts`, `".TABLE_PREFIX."threads`, `".TABLE_PREFIX."reportedcontent`");
 	
-    add_task_log($task, $lang->purgesoftdeleted_task_log);
-}
+	/**
+		* MyBB Purge soft deleted posts and threads - plugin for MyBB 1.8.x forum software
+		*
+		* @package MyBB Plugin
+		* @author MyBB Group - Eldenroot - <eldenroot@gmail.com>
+		* @copyright 2021 MyBB Group <http://mybb.group>
+		* @link <https://github.com/mybbgroup/MyBB_Purge-soft-deleted-threads-and-posts>
+		* @license GPL-3.0
+		*
+	*/
+	
+	/**
+		* This program is free software: you can redistribute it and/or modify
+		* it under the terms of the GNU General Public License as published by
+		* the Free Software Foundation, either version 3 of the License,
+		* or (at your option) any later version.
+		*
+		* This program is distributed in the hope that it will be useful,
+		* but WITHOUT ANY WARRANTY; without even the implied warranty of
+		* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+		* See the GNU General Public License for more details.
+		*
+		* You should have received a copy of the GNU General Public License
+		* along with this program.
+		* If not, see <http://www.gnu.org/licenses/>.
+	*/
+	
+	// Disallow direct access to this file for security reasons
+	if(!defined("IN_MYBB"))
+	{
+		die("Direct initialization of this file is not allowed.");
+	}
+	
+	// Purge soft deleted
+	function task_purgesoftdeleted($task)
+	{
+		global $db, $lang;
+		$lang->load("config_purgesoftdeleted");
+		
+		// Soft deleted posts and threads older than x seconds will be purged
+		$ptime = 3*24*3600; // 3 days for soft deleted posts
+		$ttime = 5*24*3600; // 5 days for soft deleted threads
+		
+		$db->delete_query("posts", "(visible = -1) AND (dateline < ".(TIME_NOW-$ptime).")");
+		
+		$db->delete_query("threads", "(visible = -1) AND (dateline < ".(TIME_NOW-$ttime).")");
+		
+		// Optimize DB table
+		$db->query("OPTIMIZE TABLE `".TABLE_PREFIX."posts`, `".TABLE_PREFIX."threads`, `".TABLE_PREFIX."reportedcontent`");
+		
+		add_task_log($task, $lang->purgesoftdeleted_task_log);
+	}
